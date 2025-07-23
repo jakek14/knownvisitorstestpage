c5c4674336bc4ec8d295414a6ac713e738ff8cec
Your Name <your-email@example.com>
2025-07-23 00:04:44 -0400

    Fix vite config and revert index.html for proper deployment

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/knownvisitorstestpage/' : '/',
+  base: '/knownvisitorstestpage/',
   plugins: [
     react(), 
     process.env.NODE_ENV === 'development' ? phionPlugin() : null
